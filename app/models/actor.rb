f62142b2070d5bb7a102ebb6d64820a9e71d1e8f
Christian Pinto <pintochristian2@gmail.com>
2019-05-27 18:32:52 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+  def full_name
+    # binding.pry
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    characters.map {|c| c.name}.first + " - " + shows.map{|s| s.name}.first
+  end
 end
